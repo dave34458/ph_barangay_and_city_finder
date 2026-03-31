ea10bdd3e2244668bd7cf56d6e8e4a035d06b63b
vinson <vinsonjoshua56@yahoo.com>
2026-04-01 06:30:28 +0800

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-import sys
-
-sys.path.append(str(Path(__file__).parent))
-import ncr_lookup
-
-features = ncr_lookup.load_ncr(Path(__file__).parent / "ncr_barangays_geojson.geojson")
-result = ncr_lookup.get_location(14.531659172292766,121.0736745605332, features)
-print(result)
